2d1224682f926d6e9bdd19621aca4b6d85c9c2eb
Maxi <maxibengochea67@gmail.com>
2025-03-13 13:06:12 -0400

    coment init app

--- src/app.py
+++ src/app.py
@@ -9,8 +9,8 @@ load_dotenv()
 
 #inicializar la app
 def create_app():
-  app = Flask(__name__)
-  app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-  app.register_blueprint(print_router)
-  CORS(app)
+  app = Flask(__name__) #crear la instancia de Flask
+  app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') #usar la secret key de la variable de entorno
+  app.register_blueprint(print_router) #registrar las rutas
+  CORS(app) #permitir peticiones de todas las rutas
   return app
